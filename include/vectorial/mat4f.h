8bfb2c320377db1c5a83d371e5a32acf16125a56
Mikko Lehtonen <scoopr@iki.fi>
2010-12-23 12:27:51 +0200

    Explicit copy-constructor for mat4f too

--- include/vectorial/mat4f.h
+++ include/vectorial/mat4f.h
@@ -24,6 +24,7 @@ namespace vectorial {
         simd4x4f value;
     
         inline mat4f() {}
+        inline mat4f(const mat4f& m) : value(m.value) {}
         inline mat4f(const simd4x4f& v) : value(v) {}
         inline mat4f(vec4f v0, vec4f v1, vec4f v2, vec4f v3) : value(simd4x4f_create(v0.value, v1.value, v2.value, v3.value)) {}
 
