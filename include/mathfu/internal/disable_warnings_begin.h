18ae8b41abe2ec8fb426553ded8b83fe6a1658d3
jsanmiya <jsanmiya@google.com>
2017-02-14 20:31:13 +0000

    Build modifications to get Zooshi to build working.
    With these changes, Zooshi will build with Blaze, CMake, and Android ndk-build.
    PiperOrigin-RevId: 147506488
    Change-Id: I28f246ebd999050c086d1d3b1145bb769867e027

--- include/mathfu/internal/disable_warnings_begin.h
+++ include/mathfu/internal/disable_warnings_begin.h
@@ -25,9 +25,11 @@
 #if defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wignored-qualifiers"
 #endif  // defined(__GNUC__)
 
 #if defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wpedantic"
+#pragma clang diagnostic ignored "-Wignored-qualifiers"
 #endif  // defined(__clang__)
