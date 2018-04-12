fb41f224526444c1a9471aeba0af7bd7386c02cb
shanee <shanee@google.com>
2018-04-12 14:12:15 -0700

    Fix vector4 simd to use 4 dimensional values for functions.
    PiperOrigin-RevId: 192667664
    Change-Id: I1cabaa77b3ced0d7b88fdd51c90da6a1ee6411d0

--- include/mathfu/internal/vector_4_simd.h
+++ include/mathfu/internal/vector_4_simd.h
@@ -257,18 +257,18 @@ class Vector<float, 4> {
     return Vector<float, 4>(simd4f_min(v1.simd, v2.simd));
   }
 
-  static inline float Distance(const Vector<float, 2>& v1,
-                               const Vector<float, 2>& v2) {
+  static inline float Distance(const Vector<float, 4>& v1,
+                               const Vector<float, 4>& v2) {
     return (v1 - v2).Length();
   }
 
-  static inline float DistanceSquared(const Vector<float, 2>& v1,
-                                      const Vector<float, 2>& v2) {
+  static inline float DistanceSquared(const Vector<float, 4>& v1,
+                                      const Vector<float, 4>& v2) {
     return (v1 - v2).LengthSquared();
   }
 
-  static inline float Angle(const Vector<float, 2>& v1,
-                            const Vector<float, 2>& v2) {
+  static inline float Angle(const Vector<float, 4>& v1,
+                            const Vector<float, 4>& v2) {
     return AngleHelper(v1, v2);
   }
 
